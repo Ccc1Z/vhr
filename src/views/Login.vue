b7e983ebdd1f915586119fcf4fc30269fdd20885
Ccc1Z <caiwentao5@qq.com>
2020-05-13 15:33:27 +0800

    login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <!--model里放的是表单数据-->
-        <el-form :rules="rules" :model="loginForm" class="loginContainer">
+        <el-form :rules="rules" ref="loginForm" :model="loginForm" class="loginContainer">
             <h3 class="loginTitle">系统登录</h3>
             <el-form-item prop="username">
                 <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="请输入用户名"></el-input>
@@ -10,7 +10,7 @@
                 <el-input type="text" v-model="loginForm.password" auto-complete="off" placeholder="请输入密码"></el-input>
             </el-form-item>
             <el-checkbox v-model="checked" class="loginRemembered"></el-checkbox>
-            <el-button type="primary" style="width: 100%">登录</el-button>
+            <el-button type="primary" style="width: 100%" @click="submitLogin">登录</el-button>
         </el-form>
     </div>
 </template>
@@ -32,6 +32,18 @@
                     username:[{required:true,message:'请输入用户名',trigger:'blur'}],
                     password:[{required:true,message:'请输入密码',trigger:'blur'}]
                 }
+            }
+        },
+        methods:{
+            submitLogin() {
+                this.$refs.loginForm.validate((valid) => {
+                    if (valid) {
+                        alert('submit!');
+                    } else {
+                        this.$message.error('请输入所有字段');
+                        return false;
+                    }
+                });
             }
         }
     }
